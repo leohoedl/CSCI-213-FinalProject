22db0b5ff198db98a4078c280f81b1629d57aecc
Leo Hoedl <leo.j.hoedl@ndus.edu>
2019-11-20 17:34:56 -0600

    Added Login functionality. Constant loading issue. Populated database as well.
    -Leo

---
+++ Login.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="FinalProject.Login" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <asp:Login ID="Login1" runat="server" BackColor="#F7F6F3" BorderColor="#E6E2D8" BorderPadding="4" BorderStyle="Solid" BorderWidth="1px" Font-Names="Verdana" Font-Size="0.8em" ForeColor="#333333" Height="275px" Width="327px" DestinationPageUrl="~/MyWork/Appointments.aspx">
+    <InstructionTextStyle Font-Italic="True" ForeColor="Black" />
+    <LoginButtonStyle BackColor="#FFFBFF" BorderColor="#CCCCCC" BorderStyle="Solid" BorderWidth="1px" Font-Names="Verdana" Font-Size="0.8em" ForeColor="#284775" />
+    <TextBoxStyle Font-Size="0.8em" />
+    <TitleTextStyle BackColor="#5D7B9D" Font-Bold="True" Font-Size="0.9em" ForeColor="White" />
+</asp:Login>
+</asp:Content>
---
+++ Login.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Security;
+
+namespace FinalProject
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            UnobtrusiveValidationMode = System.Web.UI.UnobtrusiveValidationMode.None;
+        }
+        protected void Login1_Authenticate(object sender, AuthenticateEventArgs e)
+        {
+            using (HA3_DataBaseV1Entities2 dbcon = new HA3_DataBaseV1Entities2())
+            {
+                string username = Login1.UserName;
+                string password = (from x in dbcon.UsersTables
+                                   where x.UserLoginName == username
+                                   select x.UserLoginPass).First();
+
+                if (Login1.UserName == username && Login1.Password == password)
+                {
+                    FormsAuthentication.RedirectFromLoginPage(Login1.UserName, true);
+                }
+
+            }
+        }
+
+    }
+}
